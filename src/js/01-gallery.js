431d8a1f6b36fc2347bed0ae967fd7b21e61551b
AleksandraAleksinska <aleksandra.aleksinska@gmail.com>
2023-03-02 17:59:28 +0100

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryElements = galleryItems.map((item) => 
+    `
+    <a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>
+  `
+    )
+    .join("");
+
+gallery.insertAdjacentHTML('beforeend', galleryElements);
+
+const biggerImg = new SimpleLightbox ( '.gallery a', {  
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+    });
