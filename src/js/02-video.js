61493211c3e913cdb0e96252ae60f6782d9dab92
AleksandraAleksinska <aleksandra.aleksinska@gmail.com>
2023-03-04 17:18:18 +0100

    task 2 update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
 
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('#vimeo-player');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', function (currentTime) {
+            // console.log(currentTime),
+            localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime.seconds)),
+            console.log(localStorage.getItem('videoplayer-current-time'));
+        })
+
+       player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+        .then(function(seconds) {})
+        .catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+    
+            default:
+                break;
+        }
+    });
