d8688d575d89ccaa5bd571c68e3fa52b25305a7a
AleksandraAleksinska <aleksandra.aleksinska@gmail.com>
2023-03-04 17:35:10 +0100

    task 2 update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,18 @@
 
 import Player from '@vimeo/player';
-const throttle = require('lodash.throttle');
+const _ = require('lodash');
 
 const iframe = document.querySelector('#vimeo-player');
     const player = new Player(iframe);
 
-    player.on('timeupdate', function (currentTime) {
-            // console.log(currentTime),
-            localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime.seconds)),
-            console.log(localStorage.getItem('videoplayer-current-time'));
-        })
+   const videoCurrentTime = (currentTime) => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime.seconds));
+    // console.log(localStorage.getItem('videoplayer-current-time'));
+}
 
-       player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+    player.on('timeupdate', _.throttle(videoCurrentTime, 1000));
+
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
         .then(function(seconds) {})
         .catch(function(error) {
         switch (error.name) {
